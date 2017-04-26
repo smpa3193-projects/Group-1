991d05eee20233d65cd7c0f498410c47c6d3edcb
LincolnFZL <lincolnfzl@gmail.com>
2017-04-25 21:56:31 -0400

    1_Geo_2_date

--- tweetbot2.py
+++ tweetbot2.py
@@ -1,6 +1,10 @@
 from twython import Twython
 import csv
+
 from collections import Counter
+
+import time
+ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d 
 
 CONSUMER_KEY = 'R2pWRXsbIvXqkHxljWC7muqf4'
 CONSUMER_SECRET = 'RIPuyuK8wdd81M23xDqxSWUiwcBaQO5q4TbkfiqIEAIq8T4x1C'
@@ -10,30 +14,63 @@ ACCESS_TOKEN_SECRET = 'F93y3Nn9wgaPG91iX1Gm3nPsy5jrZCa00dVIYkYabLK8I'
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 all_words = []
-tweets = twitter.search(q='#InfiniteKusama', count="500")
-text = []
 
-with open ('hashtag_tweets.csv', 'w') as fp:
+{
+    "coordinates":
+    [
+        -77.0231,
+        38.8879
+    ],
+    "type":"Point"
+}
+
+infinite_tweets = twitter.search(q='#InfiniteKusama', count=1000)
+yayoi_tweets = twitter.search(q='#YayoiKusama', count=1000)
+
+# setup containers for csvs
+inf_tweets = []
+yay_tweets = []
+
+with open ('infinite.csv', 'w') as fp:
     a = csv.writer(fp)
     # add a header row
-    a.writerow(('Hashtag', 'Tweet Text', 'URL'))
+    a.writerow(['Hashtag', 'id', 'screen_name', 'Tweet Text', 'URL'])
 
-    for result in tweets['statuses']:
+    for result in infinite_tweets['statuses']:
         try:
             url = result['entities']['urls'][0]['expanded_url']
         except:
             url = None
-        text.append(['#InfiniteKusama', result['text'].encode('utf-8'), url])
-        a.writerows((text))
+        # add tweet info to csv containers
+        inf_tweets.append(['#InfiniteKusama', result['id'], result['user']['screen_name'], result['text'].encode('utf-8'), url])
+    a.writerows(inf_tweets)
 
 # build list of words
-for tweet in tweets['statuses']:
+for tweet in infinite_tweets['statuses']:
   tweet_words = tweet['text'].split()
   all_words.append(tweet_words)
-  
+
+for tweet in yayoi_tweets['statuses']:
+  tweet_words = tweet['text'].split()
+  all_words.append(tweet_words)
+
+
+with open ('yayoikusama.csv', 'w') as fp:
+    a = csv.writer(fp)
+    # add a header row
+    a.writerow(['Hashtag', 'id', 'screen_name', 'Tweet Text', 'URL'])
+
+    for result in yayoi_tweets['statuses']:
+        try:
+            url = result['entities']['urls'][0]['expanded_url']
+        except:
+            url = None
+        yay_tweets.append(['#YayoiKusama', result['id'], result['user']['screen_name'], result['text'].encode('utf-8'), url])
+    a.writerows(yay_tweets)
+
 # this is called a list comprehension. it's weird but awesome - it performs the same action to each item in a list.
 all_words_flattened = [word.upper() for tweet_words in all_words for word in tweet_words]
 counts = Counter(all_words_flattened)
 
-for word, count in counts.most_common(200):
+for word, count in counts.most_common(15):
   print "%s: %d" % (word, count)
